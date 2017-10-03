5b7482323c4810696de45ea47c1a33cd78e675da
SKpoTH <skpoxpolice@gmail.com>
2017-10-04 01:14:50 +0700

    Get Movement UP DOWN LEFT RIGHT

--- climb_to_survive.py
+++ climb_to_survive.py
@@ -4,6 +4,9 @@ from models import World
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 900
 
+PILLAR_SCALE = 0.6
+HUMAN_SCALE = 0.2
+'''
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
@@ -17,7 +20,16 @@ class ModelSprite(arcade.Sprite):
     def draw(self):
         self.sync_with_model()
         super().draw()
+'''
 
+class Get_Sprite:
+    def __init__(self, location, model, scale):
+        self.model = model
+        self.model_sprite = arcade.Sprite(location,scale)
+
+    def draw(self):
+        self.model_sprite.set_position(self.model.x, self.model.y)
+        self.model_sprite.draw()
 
 class PillSurWindow(arcade.Window):
     def __init__(self, width, height):
@@ -27,11 +39,14 @@ class PillSurWindow(arcade.Window):
 
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.pillar_sprite1 = ModelSprite('images/pillar.png',model=self.world.pillar1)
-        self.pillar_sprite2 = ModelSprite('images/pillar.png',model=self.world.pillar2)
-        self.pillar_sprite3 = ModelSprite('images/pillar.png',model=self.world.pillar3)
-        self.pillar_sprite4 = ModelSprite('images/pillar.png',model=self.world.pillar4)
-        self.pillar_sprite5 = ModelSprite('images/pillar.png',model=self.world.pillar5)
+        self.pillar_sprite1 = Get_Sprite('images/pillar.png',self.world.pillar1,PILLAR_SCALE)
+        self.pillar_sprite2 = Get_Sprite('images/pillar.png',self.world.pillar2,PILLAR_SCALE)
+        self.pillar_sprite3 = Get_Sprite('images/pillar.png',self.world.pillar3,PILLAR_SCALE)
+        self.pillar_sprite4 = Get_Sprite('images/pillar.png',self.world.pillar4,PILLAR_SCALE)
+        self.pillar_sprite5 = Get_Sprite('images/pillar.png',self.world.pillar5,PILLAR_SCALE)
+
+        self.human_sprite = Get_Sprite('images/human_climbing.png',self.world.human_main,HUMAN_SCALE)
+
 
     def on_draw(self):
         arcade.start_render()
@@ -40,6 +55,17 @@ class PillSurWindow(arcade.Window):
         self.pillar_sprite3.draw()
         self.pillar_sprite4.draw()
         self.pillar_sprite5.draw()
+        
+        self.human_sprite.draw()
+
+    def update(self, delta):
+        self.world.update(delta)
+    
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
+    
+    def on_key_release(self, key, key_modifiers):
+        self.world.on_key_release(key, key_modifiers)
 
 def main():
     window = PillSurWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 import arcade
 
+MOVE_LENGTH = 5
+
+class Human:
+    def __init__(self, world, x, y):
+        self.x = x
+        self.y = y
+        self.change_y = 0
+        self.change_x = 0
+
+    def update(self, delta):
+        self.y += self.change_y
+        self.x += self.change_x
+
 class Pillar:
     def __init__(self, world, x, y):
         self.x = x
@@ -15,3 +28,26 @@ class World:
         self.pillar3 = Pillar(self, 450, 0)
         self.pillar4 = Pillar(self, 600, 0)
         self.pillar5 = Pillar(self, 750, 0)
+
+        self.human_main = Human(self, 450, 200)
+
+    def update(self, delta):
+        self.human_main.update(delta)
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.UP:
+            self.human_main.change_y = MOVE_LENGTH
+        if key == arcade.key.DOWN:
+            self.human_main.change_y = -MOVE_LENGTH
+        if key == arcade.key.LEFT:
+            self.human_main.x -= 150
+        if key == arcade.key.RIGHT:
+            self.human_main.x += 150
+
+
+    def on_key_release(self, key, key_modifiers):
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.human_main.change_y = 0
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.human_main.change_x = 0
+    
