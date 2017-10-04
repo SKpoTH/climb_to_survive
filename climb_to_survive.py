0a3c3d7dac9306ae72ff2b15b4ec255cfc8b7429
SKpoTH <skpoxpolice@gmail.com>
2017-10-04 22:13:12 +0700

    Recoding to co-model-sprite in one class

--- climb_to_survive.py
+++ climb_to_survive.py
@@ -6,61 +6,31 @@ SCREEN_WIDTH = 900
 
 PILLAR_SCALE = 0.6
 HUMAN_SCALE = 0.2
-'''
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
- 
-        super().__init__(*args, **kwargs)
- 
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
- 
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
-'''
-
-class Get_Sprite:
-    def __init__(self, location, model, scale):
-        self.model = model
-        self.model_sprite = arcade.Sprite(location,scale)
-
-    def draw(self):
-        self.model_sprite.set_position(self.model.x, self.model.y)
-        self.model_sprite.draw()
+SPIDER_SCALE = 0.2
 
 class PillSurWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.WHITE)
-
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-        self.pillar_sprite1 = Get_Sprite('images/pillar.png',self.world.pillar1,PILLAR_SCALE)
-        self.pillar_sprite2 = Get_Sprite('images/pillar.png',self.world.pillar2,PILLAR_SCALE)
-        self.pillar_sprite3 = Get_Sprite('images/pillar.png',self.world.pillar3,PILLAR_SCALE)
-        self.pillar_sprite4 = Get_Sprite('images/pillar.png',self.world.pillar4,PILLAR_SCALE)
-        self.pillar_sprite5 = Get_Sprite('images/pillar.png',self.world.pillar5,PILLAR_SCALE)
-
-        self.human_sprite = Get_Sprite('images/human_climbing.png',self.world.human_main,HUMAN_SCALE)
-
 
     def on_draw(self):
         arcade.start_render()
-        self.pillar_sprite1.draw()
-        self.pillar_sprite2.draw()
-        self.pillar_sprite3.draw()
-        self.pillar_sprite4.draw()
-        self.pillar_sprite5.draw()
+        self.world.pillar1.draw()
+        self.world.pillar2.draw()
+        self.world.pillar3.draw()
+        self.world.pillar4.draw()
+        self.world.pillar5.draw()
         
-        self.human_sprite.draw()
+        self.world.human_main.draw()
+        
+        for spider in self.world.spider_list:
+            spider.draw()
 
     def update(self, delta):
         self.world.update(delta)
-    
+
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
     
--- models.py
+++ models.py
@@ -1,71 +1,110 @@
 import arcade
+from random import randint
 
-MOVE_LENGTH = 5
+HUMAN_MOVE_LENGTH = 5
 PILLAR_FARTHUR = 150 #the Range between a pillar to a pillar
+SPIDER_MOVE = 2
 
-class Human:
-    def __init__(self, world, x, y):
+PILLAR_SCALE = 0.6
+HUMAN_SCALE = 0.2
+SPIDER_SCALE = 0.2
+
+SPAWN_TIME = 1.0
+
+class Human(arcade.Sprite):
+    def setup(self, world, x, y):
         self.world = world
-        self.x = x
-        self.y = y
+        self.center_x = x
+        self.center_y = y     
         self.change_y = 0
 
     def update(self, delta):
-        self.y += self.change_y
+        self.center_y += self.change_y
 
         #Block player to not go outside playing area
-        if self.y >= self.world.height or self.y <= 0:
+        if self.center_y >= self.world.height or self.center_y <= 0:
             self.change_y = 0
-        if self.x >= PILLAR_FARTHUR*5:
-            self.x = PILLAR_FARTHUR*5
-        elif self.x <= PILLAR_FARTHUR*1:
-            self.x = PILLAR_FARTHUR*1
+        if self.center_x >= PILLAR_FARTHUR*5:
+            self.center_x = PILLAR_FARTHUR*5
+        elif self.center_x <= PILLAR_FARTHUR*1:
+            self.center_x = PILLAR_FARTHUR*1
 
-class Pillar:
-    def __init__(self, world, x, y):
-        self.x = x
-        self.y = y
+class Pillar(arcade.Sprite):
+    def setup(self, world, x, y):
+        self.center_x = x
+        self.center_y = y
+
+class Spider_Enemy(arcade.Sprite):
+    def setup(self, world, x, y):
+        self.center_x = x
+        self.center_y = y
+
+    def update(self, delta): 
+        self.center_y -= SPIDER_MOVE
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.wait = 0
         
-        self.pillar1 = Pillar(self, PILLAR_FARTHUR*1, 0)
-        self.pillar2 = Pillar(self, PILLAR_FARTHUR*2, 0)
-        self.pillar3 = Pillar(self, PILLAR_FARTHUR*3, 0)
-        self.pillar4 = Pillar(self, PILLAR_FARTHUR*4, 0)
-        self.pillar5 = Pillar(self, PILLAR_FARTHUR*5, 0)
+        self.pillar1 = Pillar('images/pillar.png', PILLAR_SCALE)
+        self.pillar1.setup(self, PILLAR_FARTHUR*1, 0)
 
-        self.human_main = Human(self, PILLAR_FARTHUR*3, height/2)
+        self.pillar2 = Pillar('images/pillar.png', PILLAR_SCALE)
+        self.pillar2.setup(self, PILLAR_FARTHUR*2, 0)
+
+        self.pillar3 = Pillar('images/pillar.png', PILLAR_SCALE)
+        self.pillar3.setup(self, PILLAR_FARTHUR*3, 0)
+        
+        self.pillar4 = Pillar('images/pillar.png', PILLAR_SCALE)
+        self.pillar4.setup(self, PILLAR_FARTHUR*4, 0)
+        
+        self.pillar5 = Pillar('images/pillar.png', PILLAR_SCALE)
+        self.pillar5.setup(self, PILLAR_FARTHUR*5, 0)
+
+        self.human_main = Human('images/human_climbing.png', HUMAN_SCALE)
+        self.human_main.setup(self, PILLAR_FARTHUR*3, height/2)
+
+        self.spider_list = []
 
     def update(self, delta):
         self.human_main.update(delta)
+        
+        self.wait += delta
+        if self.wait >= SPAWN_TIME:
+            self.spider = Spider_Enemy('images/spider_enemy.png', SPIDER_SCALE)
+            self.spider.setup(self, PILLAR_FARTHUR*randint(1,5), self.height)
+            self.spider_list.append(self.spider)
+            self.wait -= SPAWN_TIME
+        
+        for spider in self.spider_list:
+            spider.update(delta)
+            if spider.center_y <= 0:
+                self.spider_list.remove(spider)
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.UP:
-            self.human_main.change_y = MOVE_LENGTH
-            if self.human_main.y >= self.height:
+            self.human_main.change_y = HUMAN_MOVE_LENGTH
+            if self.human_main.center_y >= self.height:
                 self.human_main.change_y = 0
             #If state below key I place for blocking player to spam key and player can go out
 
         if key == arcade.key.DOWN:
-            self.human_main.change_y = -MOVE_LENGTH
-            if self.human_main.y <= 0:
+            self.human_main.change_y = -HUMAN_MOVE_LENGTH
+            if self.human_main.center_y <= 0:
                 self.human_main.change_y = 0
 
         if key == arcade.key.LEFT:
-            self.human_main.x -= PILLAR_FARTHUR
-            if self.human_main.x <= PILLAR_FARTHUR*1:
-                self.human_main.x -= 0
+            self.human_main.center_x -= PILLAR_FARTHUR
+            if self.human_main.center_x <= PILLAR_FARTHUR*1:
+                self.human_main.center_x -= 0
 
         if key == arcade.key.RIGHT:
-            self.human_main.x += PILLAR_FARTHUR
-            if self.human_main.x >= PILLAR_FARTHUR*5:
-                self.human_main.x += 0
-
+            self.human_main.center_x += PILLAR_FARTHUR
+            if self.human_main.center_x >= PILLAR_FARTHUR*5:
+                self.human_main.center_x += 0
 
     def on_key_release(self, key, key_modifiers):
         if key == arcade.key.UP or key == arcade.key.DOWN:
             self.human_main.change_y = 0
-    
